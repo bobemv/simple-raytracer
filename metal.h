f09d78ef5e4b788bd96a5acc1b6cb7c3c8630c58
bobemv <bobemv@gmail.com>
2020-01-04 20:15:33 +0100

    Added fuzziness to metal materials (rays reflected are randomized depending on fluziness)

--- metal.h
+++ metal.h
@@ -7,11 +7,13 @@
 class metal : public material {
 public:
     vec3 albedo;
+    float fuzz;
 public:
-    metal(const vec3& a) : albedo(a) {};
+    metal(const vec3& a, float f) : albedo(a) { if (f < 1) fuzz = f; else fuzz = 1; };
     virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const {
+        randomize* randomize_gen = &randomize::get_instance();
         vec3 reflected = reflect(r_in.direction().unit_vector(), rec.normal);
-        scattered = ray(rec.p, reflected);
+        scattered = ray(rec.p, reflected + fuzz*randomize_gen->random_in_unit_sphere());
         attenuation = albedo;
         return scattered.direction().dot(rec.normal) > 0;
     }
